c102c9fbd45608d2fe0103f73ebf7a57a13dec3c
benacq <benacq44@gmail.com>
2021-06-26 11:48:54 +0000

    Updates

---
+++ precache-manifest.5a3a050dc0b71595c2eb87010d3ca35f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49cbaf98682b40c65fba0243d378f1f",
+    "url": "/fidelis/index.html"
+  },
+  {
+    "revision": "dc1e18b8929fdc2985bf",
+    "url": "/fidelis/static/css/main.3be2973d.chunk.css"
+  },
+  {
+    "revision": "69e48598116b9a09d072",
+    "url": "/fidelis/static/js/2.d14ce67e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fidelis/static/js/2.d14ce67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1e18b8929fdc2985bf",
+    "url": "/fidelis/static/js/main.e9db168e.chunk.js"
+  },
+  {
+    "revision": "2b31b642aadb02bdb446",
+    "url": "/fidelis/static/js/runtime-main.66686ef8.js"
+  },
+  {
+    "revision": "751e58bb00f17b10a8eee65345103ba2",
+    "url": "/fidelis/static/media/fidelis.751e58bb.jpg"
+  }
+]);
